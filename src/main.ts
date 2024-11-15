b47ab262d76aab658f6d3c71032476067da5bba0
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-11-16 00:00:14 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -51,5 +51,20 @@ fetchUserData('123', (data) => {
   console.log(data.name); // TypeScript не викличе помилку, навіть якщо поле name не існує
 });
 
-export {};
+export { };
+  
+  function fetchUserata(id: string, callback: (data: any) => void): void {
+  // Тут може бути якийсь запит, але ми його заповнимо самі
+  const responseData = { name: 'Tom' };
+
+  callback(responseData);
+}
+
+// Використання функції:
+fetchUserata('123', (data) => {
+  console.log(data.name); // TypeScript не викличе помилку, навіть якщо поле name не існує
+});
+
+export { };
+  
 
